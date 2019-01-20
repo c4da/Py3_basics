22400f951d808caed51957de1e4378702dfaa4ca
Miloš Čadek <34865543+c4da@users.noreply.github.com>
2019-01-20 23:41:16 +0100

    Add files via upload
    python 36, simple class inheritance syntax example

---
+++ class_test_atributes_inheritance.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 13 11:10:01 2018
+
+class test
+
+@author: MCA
+"""
+
+
+class First(object):
+    
+    def __init__(self):
+        
+        self.A = "A"
+    
+    def method_first(self):
+        
+        print("method first", self.A)
+    
+class Second(First):
+    
+    def __init__(self, First):
+        First.__init__(self)
+        self.B = "B"
+        self.method_sec()
+        
+    def method_sec(self):
+        
+        print(self.A)
+        print(self.B)
+
+        
+def main():
+    
+    create_obj_class_first = First()
+    create_obj_class_first.method_first()
+    create_obj_class_sec = Second(First)
+    create_obj_class_sec.method_sec()
+    create_obj_class_sec.method_first()
+    
+if __name__ == "__main__":
+    main()
